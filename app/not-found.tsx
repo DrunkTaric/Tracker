41e4055ee69be879768441160b7fe0259774e179
comborush <comborush.init@proton.me>
2024-06-24 21:39:01 +0300

    not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <main className="h-full flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold">Not Found</h1>
+      <Image
+        src="/cute.png"
+        alt="Cute golang image"
+        width={500} height={500}
+      />
+      <h1>the page you trying to reach doesn't exist</h1>
+    </main>
+  )
+}
