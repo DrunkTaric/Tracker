cf7c4ad7717d404e291bdb0fde19304473ae5fd1
comborush <comborush.init@proton.me>
2024-06-24 13:20:32 +0300

    added the main funtionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="">
+    <main className="h-full flex flex-col justify-center p-28">
+      <h1 className="text-8xl font-bold text-[var(--primary)]">Welcome</h1>
+      <h1 className="text-7xl font-bold text-[var(--primary)]">Explore our <Link href="/tasks" className="hover:bg-gradient-to-r hover:from-[var(--secondary)] hover:to-[var(--accent)] hover:bg-clip-text hover:text-transparent transition-all duration-1000">App</Link></h1>
     </main>
   );
 }
