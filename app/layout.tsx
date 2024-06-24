48a74f1a3c3a26bec5afa9caa646ff815203239c
comborush <comborush.init@proton.me>
2024-06-24 21:39:30 +0300

    addin the query that i don't use anyway

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
+import ReactQuery from "@/provider/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
       <body className={inter.className + " h-screen w-screen"}>
         <Nav />
         <div className="h-full">
-          {children}
+          <ReactQuery>
+            {children}
+          </ReactQuery>
         </div>
       </body>
     </html>
