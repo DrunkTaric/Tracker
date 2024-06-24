cffd750ac40946950cc1bf96dc6a3ce4159ba94d
comborush <comborush.init@proton.me>
2024-06-24 21:39:51 +0300

    tasks page

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,15 +1,33 @@
 "use client"
 
 import Link from "next/link"
-import { useState } from "react";
+import { Task as TaskT } from "@prisma/client";
+
 import { IoIosLink } from "react-icons/io";
 import { IoIosRemoveCircleOutline } from "react-icons/io";
 
-const Task = ({ text }: { text: string }) => {
+import { Authenticate, isAuthenticated } from "@/functions/auth";
+import { useMutation } from "react-query";
+import { useEffect, useState } from "react";
+
+const Task = ({ item }: { item: TaskT & { isLoading?: boolean } }) => {
+  if (item.isLoading) {
+    return (
+      <li className="flex bg-[var(--primary)] border-4 border-gray-600 rounded-xl w-full p-2 rounded-2xl text-gray-500 hover:text-gray-700">
+        <div className="flex w-[90%]">
+          <div className="mt-auto mb-auto h-[60%] w-[50%] animate-pulse bg-gray-600 bg-opacity-50 rounded-xl"></div>
+        </div>
+        <section className="flex h-full">
+          <button><IoIosRemoveCircleOutline className="text-2xl text-red-500 mt-auto" /></button>
+          <button><IoIosLink className="text-2xl text-green-500 mb-auto" /></button>
+        </section>
+      </li>
+    )
+  }
   return (
     <li className="bg-[var(--primary)] border-4 border-gray-600 rounded-xl w-full p-2 rounded-2xl text-gray-500 hover:text-gray-700">
-      <Link className="flex space-x-2 items-center justify-center" href="">
-        <h1 className="w-[90%]">{text}</h1>
+      <Link className="flex space-x-2 items-center justify-center" href={`/tasks/${item.id}`}>
+        <h1 className="w-[90%]">{item.text}</h1>
         <section className="flex h-full">
           <button><IoIosRemoveCircleOutline className="text-2xl text-red-500 mt-auto" /></button>
           <button><IoIosLink className="text-2xl text-green-500 mb-auto" /></button>
@@ -19,37 +37,47 @@ const Task = ({ text }: { text: string }) => {
   )
 }
 
-interface TaskT {
-  id: number | null,
-  text: string,
-  description: string,
-  notes: string[]
-}
-
 export default function Tasks() {
 
-  const [CurrentTasks, setCurrentTasks] = useState<TaskT[]>([])
+  const [currentTasks, setCurrentTasks] = useState<TaskT[]>([])
 
-  function AddTask(newTask: TaskT) {
-    setCurrentTasks([newTask, ...CurrentTasks])
+  const { isError, isLoading, data, mutate } = useMutation({
+    mutationFn: async () => {
+      return await fetch(`/api/tasks?session=${localStorage.getItem("session")}`).then((res) => res.json())
+    },
+    onSuccess(data, variables, context) {
+      setCurrentTasks(data.tasks.reverse())
+    },
+  })
+
+  useEffect(() => {
+    mutate()
+  }, [])
+
+  async function AddTask(text: string) {
+    if (!isAuthenticated()) { await Authenticate() }
+    setCurrentTasks([{ isLoading: true }, ...currentTasks])
+    await fetch(`/api/tasks?session=${localStorage.getItem("session")}&text=${text}`, { method: "POST" })
+    mutate()
   }
 
   return (
     <main className="h-full flex flex-col items-center justify-center">
-      <section className="w-[40rem] h-[40rem] space-y-2">
+      <section className="w-[40rem] h-[40rem] space-y-3">
         <div className="flex bg-[var(--primary)] w-full p-2 rounded-2xl">
           <input maxLength={75} className="w-full h-8 border-2 border-gray-600 outline-none p-5 rounded-2xl m-auto bg-transparent" placeholder="Type here" onKeyUp={(event) => {
             event.preventDefault()
             if (event.key != "Enter") { return }
             const input_element = (event.target as HTMLInputElement)
             if (input_element.value.length <= 0) { return }
-            AddTask({ id: null, text: input_element.value, description: "", notes: [] } satisfies TaskT)
+            AddTask(input_element.value)
+            input_element.value = ""
           }} />
         </div>
-        <ol className="h-full">
+        <ol className="appearance-none h-full space-y-1 overflow-y-auto">
           {
-            CurrentTasks.map((item, index) => {
-              return <Task key={index} text={item.text} />
+            currentTasks.map((item: TaskT, index: number) => {
+              return <Task key={index} item={item} />
             })
           }
         </ol>
