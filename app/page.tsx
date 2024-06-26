3ab08ccda96445c78517c62ef3c443f730d67bbd
comborush <comborush.init@proton.me>
2024-06-26 16:42:09 +0300

    Made it responsive (Kinda)

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="h-full flex flex-col justify-center p-28">
-      <h1 className="text-8xl font-bold text-[var(--primary)]">Welcome</h1>
-      <h1 className="text-7xl font-bold text-[var(--primary)]">Explore our <Link href="/tasks" className="hover:bg-gradient-to-r hover:from-[var(--secondary)] hover:to-[var(--accent)] hover:bg-clip-text hover:text-transparent transition-all duration-1000">App</Link></h1>
+    <main className="h-full flex flex-col justify-center items-center p-10 md:p-28">
+      <h1 className="text-5xl md:text-7xl font-bold text-[var(--primary)]">Welcome</h1>
+      <h1 className="text-4xl md:text-6xl font-bold text-[var(--primary)]">Explore our <Link href="/tasks" className="bg-gradient-to-r from-[var(--secondary)] to-[var(--accent)] bg-clip-text text-transparent">App</Link></h1>
     </main>
   );
 }
