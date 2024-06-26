80adb89753f99a5a45b22ec4cbd519f56de38237
comborush <comborush.init@proton.me>
2024-06-26 23:15:26 +0300

    the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
 import ReactQuery from "@/provider/react-query";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body className={inter.className + " h-screen w-screen"}>
         <Nav />
         <div className="h-full">
